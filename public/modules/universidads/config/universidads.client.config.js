6bcadfa222a3daf0a910708f4dba476e7c1d3e66
Mariano Queirel <mqueirel@corrientes.gov.ar>
2015-07-29 08:20:21 -0300

    add Filter and change lenguage

--- public/modules/universidads/config/universidads.client.config.js
+++ public/modules/universidads/config/universidads.client.config.js
@@ -4,7 +4,7 @@
 angular.module('universidads').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Universidads', 'universidads', 'dropdown', '/universidads(/create)?');
+		Menus.addMenuItem('topbar', 'Universidades', 'universidads', 'dropdown', '/universidads(/create)?');
 		Menus.addSubMenuItem('topbar', 'universidads', 'List Universidads', 'universidads');
 		Menus.addSubMenuItem('topbar', 'universidads', 'New Universidad', 'universidads/create');
 	}
